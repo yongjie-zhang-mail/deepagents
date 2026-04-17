6410a7aeabecbb7131c4f287c35f9176b1a6afc4
yongjie <yongjie_zhang_mail@qq.com>
2026-04-17 21:00:37 +0800

    update(examples/deep_research/agent): add deepseek model, HITL, etc.

--- examples/deep_research/agent.py
+++ examples/deep_research/agent.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from langchain.chat_models import init_chat_model
 from langchain_google_genai import ChatGoogleGenerativeAI
 from deepagents import create_deep_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 from research_agent.prompts import (
     RESEARCHER_INSTRUCTIONS,
@@ -44,16 +45,28 @@ research_sub_agent = {
     "tools": [tavily_search, think_tool],
 }
 
-# Model Gemini 3 
+# Model Gemini 3
 # model = ChatGoogleGenerativeAI(model="gemini-3-pro-preview", temperature=0.0)
 
 # Model Claude 4.5
-model = init_chat_model(model="anthropic:claude-sonnet-4-5-20250929", temperature=0.0)
+# model = init_chat_model(model="anthropic:claude-sonnet-4-5-20250929", temperature=0.0)
+
+# Model DeepSeek
+model = init_chat_model(model="deepseek-chat", temperature=0.0)
+
+
+# Checkpointer is REQUIRED for human-in-the-loop
+# checkpointer = MemorySaver()
+checkpointer = None
 
 # Create the agent
 agent = create_deep_agent(
     model=model,
     tools=[tavily_search, think_tool],
+    interrupt_on={
+        "write_file": True,  # Default: approve, edit, reject
+    },
+    checkpointer=checkpointer,  # Required!
     system_prompt=INSTRUCTIONS,
     subagents=[research_sub_agent],
 )
